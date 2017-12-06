62ded7658b1b151b5bd403311fd4558ce5fe13e1
Bryan Frey <bryanfrey@Bryans-MacBook-Pro.local>
2017-12-06 14:36:56 -0800

    update

--- js/main.js
+++ js/main.js
@@ -2,86 +2,134 @@
 	{
 		sign: "aries",
 		animal: "rat",
-		description: "Active, Demanding, Determined, Effective, Ambitious",
-		fortune: "Intelligent, adaptable, quick-witted, charming, artistic, sociable",
-		birthstone: "Garnet",
+		description: "Aries: active, demanding, determined, effective, ambitious",
+		fortune: "Rat: intelligent, adaptable, quick-witted, charming, artistic, sociable",
+		birthmonth: "january",
+		stone: "Garnet",
+		birthstone: "img/garnet.jpg",
+		picture: "img/aries.jpg",
+		chinesepic: "img/rat.jpg"
 	},
 	{
 		sign: "taurus",
 		animal: "ox",
-		description: "Security, Subtle strength, Appreciation, Instruction, Patience",
-		fortune: "Loyal, reliable, thorough, strong, reasonable, steady, determined",
-		birthstone: "Aquamarine",
+		description: "Taurus: security, subtle strength, appreciation, instruction, patience",
+		fortune: "Ox: loyal, reliable, thorough, strong, reasonable, steady, determined",
+		birthmonth: "march",
+		stone: "Aquamarine",
+		birthstone: "img/aquamarine.jpg",
+		picture: "img/taurus.jpg",
+		chinesepic: "img/taurus.jpg"
 	},
 	{
 		sign: "gemini",
 		animal: "tiger",
-		description: "Communication, Indecision, Inquisitive, Intelligent, Changeable",
-		fortune: "Enthusiastic, courageous, ambitious, leadership, confidence, charismatic",
-		birthstone: "Sapphire",
+		description: "Gemini: communication, indecision, inquisitive, intelligent, changeable",
+		fortune: "Tiger: enthusiastic, courageous, ambitious, leadership, confidence, charismatic",
+		birthmonth: "september",
+		stone: "Sapphire",
+		birthstone: "img/sapphire.jpg",
+		picture: "img/gemini.jpg",
+		chinesepic: "img/tiger.jpg"
 	},
 	{
 		sign: "cancer",
 		animal: "rabbit",
-		description: "Emotion, Diplomatic, Intensity, Impulsive, Selective",
-		fortune: "Trustworthy, empathic, modest, diplomatic, sincere, sociable, caretakers",
-		birthstone: "Pearl",
+		description: "Cancer: emotion, diplomatic, intensity, impulsive, selective",
+		fortune: "Rabbit: trustworthy, empathic, modest, diplomatic, sincere, sociable, caretakers",
+		birthmonth: "june",
+		stone: "Pearl",
+		birthstone: "img/pearl.jpg",
+		picture: "img/cancer.jpg",
+		chinesepic: "img/rabbit.jpg"		
 	},
 	{
 		sign: "leo",
 		animal: "dragon",
-		description: "Ruling, Warmth, Generosity, Faithful, Initiative",
-		fortune: "Lucky, flexible, eccentric, imaginative, artistic, spiritual, charismatic",
-		birthstone: "Amethyst",
+		description: "Leo: ruling, warmth, generosity, faithful, initiative",
+		fortune: "Dragon: lucky, flexible, eccentric, imaginative, artistic, spiritual, charismatic",
+		birthmonth: "february",
+		stone: "Amethyst",
+		birthstone: "img/amethyst.jpg",
+		picture: "img/leo.jpg",
+		chinesepic: "img/dragon.jpg"
 	},
 	{
 		sign: "virgo",
 		animal: "snake",
-		description: "Analyzing, Practical, Reflective, Observation, Thoughtful",
-		fortune: "Philosophical, organized, intelligent, intuitive, elegant, attentive, decisive",
-		birthstone: "Opal",
+		description: "Virgo: analyzing, practical, reflective, observation, thoughtful",
+		fortune: "Snake: philosophical, organized, intelligent, intuitive, elegant, attentive, decisive",
+		birthmonth: "october",
+		stone: "Opal",
+		birthstone: "img/opal.jpg",
+		picture: "img/virgo.jpg",
+		chinesepic: "img/snake.jpg"
 	},
 	{
 		sign: "libra",
 		animal: "horse",
-		description: "Balance, Justice, Truth, Beauty, Perfection",
-		fortune: "Adaptable, loyal, courageous, ambitious, intelligent, adventurous, strong",
-		birthstone: "Topaz",
+		description: "Libra: balance, justice, truth, beauty, perfection",
+		fortune: "Horse: adaptable, loyal, courageous, ambitious, intelligent, adventurous, strong",
+		birthmonth: "november",
+		stone: "Topaz",
+		birthstone: "img/topaz.jpg",
+		picture: "img/libra.jpg",
+		chinesepic: "img/horse.jpg"
 	},
 	{
 		sign: "scorpio",
 		animal: "goat",
-		description: "Transient, Self-Willed, Purposeful, Unyielding",
-		fortune: "Tasteful, crafty, warm, elegant, charming, intuitive, sensitive, calm",
-		birthstone: "Emerald",
+		description: "Scorpio: transient, self-willed, purposeful, unyielding",
+		fortune: "Goat: tasteful, crafty, warm, elegant, charming, intuitive, sensitive, calm",
+		birthmonth: "may",
+		stone: "Emerald",
+		birthstone: "img/emerald.jpg",
+		picture: "img/scorpio.jpg",
+		chinesepic: "img/goat.jpg"
 	},
 	{
 		sign: "sagittarius",
 		animal: "monkey",
-		description: "Philosophical, Motion, Experimentation, Optimism",
-		fortune: "Quick-witted, charming, lucky, adaptable, bright, versatile, lively, smart",
-		birthstone: "Peridot",
+		description: "Sagittarius: philosophical, motion, experimentation, optimism",
+		fortune: "Monkey: quick-witted, charming, lucky, adaptable, bright, versatile, lively, smart",
+		birthmonth: "august",
+		stone: "Peridot",
+		birthstone: "img/peridot.jpg",
+		picture: "img/sagittarius.jpg",
+		chinesepic: "img/monkey.jpg"
 	},
 	{
 		sign: "capricorn",
 		animal: "rooster",
-		description: "Determination, Dominance, Perservering, Practical, Willful",
-		fortune: "Honest, energetic, intelligent, flamboyant, flexible, diverse, confident",
-		birthstone: "Citrine",
+		description: "Capricorn: determination, dominance, perservering, practical, willful",
+		fortune: "Rooster: honest, energetic, intelligent, flamboyant, flexible, diverse, confident",
+		birthmonth: "december",
+		stone: "Tanzanite",
+		birthstone: "img/tanzanite.jpg",
+		picture: "img/capricorn.jpg",
+		chinesepic: "img/rooster.jpg"
 	},
 	{
 		sign: "aquarius",
 		animal: "dog",
-		description: "Knowledge, Humanitarian, Serious, Insightful, Duplicitous",
-		fortune: "Loyal, sociable, courageous, diligent, steady, lively, adaptable, smart",
-		birthstone: "Diamond",
+		description: "Aquarius: knowledge, humanitarian, serious, insightful, duplicitous",
+		fortune: "Dog: loyal, sociable, courageous, diligent, steady, lively, adaptable, smart",
+		birthmonth: "april",
+		stone: "Diamond",
+		birthstone: "img/diamond.jpg",
+		picture: "img/aquarius.jpg",
+		chinesepic: "img/dog.jpg"
 	},
 	{
 		sign: "pisces",
 		animal: "pig",
-		description: "Fluctuation, Depth, Imagination, Reactive, Indecisive",
-		fortune: "Honorable, philanthropic, determined, optimistic, sincere, sociable",
-		birthstone: "Ruby",
+		description: "Pisces: fluctuation, depth, imagination, reactive, indecisive",
+		fortune: "Pig: honorable, philanthropic, determined, optimistic, sincere, sociable",
+		birthmonth: "july",
+		stone: "Ruby",
+		birthstone: "img/ruby.jpg",
+		picture: "img/pisces.jpg",
+		chinesepic: "img/pig.jpg"
 	}
 ]
 
@@ -89,13 +137,73 @@ function getInfo() {
 	var sign = document.getElementById("sign").value
 	for(i=0; i<signObject.length; i++) {
 		if (sign == signObject[i].sign) {
-			console.log("Your Zodiac characteristics: " + signObject[i].description)
+			document.getElementById("allsigns").innerHTML="Your Zodiac sign is a " + signObject[i].description
+			document.getElementById('image').src = signObject[i].picture
 		return
-		}
-		if (sign == signObject[i].animal) {
-			console.log("Your Chinese fortune: " + signObject[i].fortune)
-		return
+		} else {
+			document.getElementById("allsigns").innerHTML="Not a zodiac sign"
 		}
 	}
-		alert("Not a zodiac or chinese sign")
 }
+function animalInfo() {
+	var animal = document.getElementById("animal").value
+	for(i=0; i<signObject.length; i++) {
+		if (animal == signObject[i].animal) {
+			document.getElementById("allsigns").innerHTML="Your Chinese animal is a " + signObject[i].fortune
+			document.getElementById('image').src = signObject[i].chinesepic
+		return
+		} else {
+			document.getElementById("allsigns").innerHTML="Not an animal sign"
+		}
+	}
+}
+function birthInfo() {
+	var birthmonth = document.getElementById("birthmonth").value
+	for(i=0; i<signObject.length; i++) {
+		if (birthmonth == signObject[i].birthmonth) {
+			document.getElementById("allsigns").innerHTML="Your birthstone is " + signObject[i].stone
+			document.getElementById('image').src = signObject[i].birthstone
+		return
+		} else {
+			document.getElementById("allsigns").innerHTML="Not a birth month sign"
+		}
+	}
+}
+
+
+function clearBox(birthInfo)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+
+// function myFunction() {
+// 	document.getElementById('myForm').reset();
+// }
+
+//	Enter Key
+// document.onkeydown = function (a) {
+// 	if(a.keyCode == 13) {
+// 		a.preventDefault();
+// 	if(the element clicked is '#sign') {
+	
+// 	}
+
+
+	
+// 	if (the element clicked is '#news_gallery li .over') {
+//     var article = $('#news-article .news-article');
+// } else if (the element clicked is '#work_gallery li .over') {
+//     var article = $('#work-article .work-article');
+// } else if (the element clicked is '#search-item li') {
+//     var article = $('#search-item .search-article');
+// };
+// 		getInfo();
+// 	}
+// 	}
+// ;
+
+
+// Find out element user clicks
+//Pass through keydown
+//Compare based on input that fires off the matching function
